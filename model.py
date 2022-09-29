a0ff0a6300ff0dffce159f168729b8928de3a9c4
Nathan Pretorius <npretor@g.clemson.edu>
2022-09-29 15:05:16 -0700

    initial model design and agent wireframe

---
+++ model.py
@@ -0,0 +1,19 @@
+import torch 
+import torch.nn as nn
+import torch.nn.functional as F
+
+class DDPG_model(nn.Module):
+    def __init__(self, input_size, output_size, seed) -> None:
+        super(DDPG_model, self).__init__()
+        self.seed = torch.manual_seed(seed)
+
+        self.fc1 = torch.nn.Linear(input_size, 64)
+        self.fc2 = torch.nn.Linear(64, 64)
+        self.fc3 = torch.nn.Linear(64, output_size)
+
+    def forward(self, state): 
+        x = self.fc1(state)
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = F.relu(x)
+        return x 
