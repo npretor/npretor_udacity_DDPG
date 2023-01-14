f728e9df7f6d92263c4438913bf6faf2baff1512
Nathan Pretorius <npretor@g.clemson.edu>
2023-01-13 20:20:41 -0800

    setup file for activating workspace

---
+++ setup.sh
@@ -0,0 +1,4 @@
+cd /Users/nathan/Documents/Learning/Udacity_deepRL
+source mlagents_venv/bin/activate 
+cd /Users/nathan/Documents/repositories/deep-reinforcement-learning/p2_continuous-control 
+conda activate drlnd
